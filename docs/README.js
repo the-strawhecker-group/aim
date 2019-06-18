36329265bd2c8b60033b1410543d5c3bbefa1370
Jacob Hayes <jacob.r.hayes@gmail.com>
2019-06-17 23:04:52 -0500

    Fix js var ref and config result

--- docs/README.js
+++ docs/README.js
@@ -1,9 +1,12 @@
-document.addEventListener("load", function() {
+document.addEventListener("DOMContentLoaded", function() {
    var access_token_url = document.getElementById("access-token-url"),
        discovery_url = document.getElementById("api-discovery-config-url"),
        discovery_config = document.getElementById("api-discovery-config");
-   fetch(l.href)
+   fetch(discovery_url.href)
      .then(res => res.json())
-     .then((data) => {discovery_config.innerHTML = data; access_token_url.innerHTML = data.urls.accessToken})
+     .then((data) => {
+        discovery_config.innerHTML = JSON.stringify(data);
+        access_token_url.innerHTML = data.urls.accessToken;
+     })
      .catch(err => { throw err });
 });
